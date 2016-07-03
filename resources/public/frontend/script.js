f3a5fca4363d8b855d2fd497569008d360055ff9
Hugo <hugo.hornquist@gmail.com>
2016-07-03 18:05:43 +0200

    Cleaned up js and html

--- resources/public/frontend/script.js
+++ resources/public/frontend/script.js
@@ -147,38 +147,6 @@ var afterInit = function() {
 
 
 $(document).ready(function() {
-	// Sets up note request fields
-	$("#get-note-button").click(function () {
-		getNotesById($("#node-id-input").val());
-	});
-	$("#node-id-input").keypress(function (e) {
-		if(e.which == 13) { getNotesById($("#node-id-input").val()); }
-	});
-
-	// Sets up "Clear" button
-	$("#clear-notes-button").click(function() {
-		$("#note-container").empty();
-	});
-
-	// adds authorization key to POST form
-	// TODO this should probably be run upon submit
-	// TODO since it currently requires you to submit the note
-	// TODO quite soon after opening the page
-	$.get("/note/token/html", function (data) {
-		$("#comment-form").append(data);
-	});
-
-	/*
-	// this still publishes the data to the "value" url
-	// but runs this function instead of redirecting
-	$("#comment-form").ajaxForm(function(data) {
-		// Data is the server response
-		JSON.parse(data).map(function(key) {
-			getNotesById(key.generated_key);
-		});
-	});
-	*/
-
 	// add all notes from the server to start with
 	getNotes("/note/all");
 
