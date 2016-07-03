0bad1428acb35a1f8780da6dc243d5a3a7a45ee0
Hugo <hugo.hornquist@gmail.com>
2016-07-03 14:07:30 +0200

    changed all url's by removing 'http://localhost:3000'

--- resources/public/frontend/script.js
+++ resources/public/frontend/script.js
@@ -1,7 +1,7 @@
 // TODO possibly only require a sub url
 var post = function(url, json, callback) {
 	// TODO can I handle the anti forgery token like this?
-	$.get("http://localhost:3000/note/token/raw", function(token) {
+	$.get("/note/token/raw", function(token) {
 		json["__anti-forgery-token"] = token;
 		$.post(url, json, callback);
 	});
@@ -21,7 +21,7 @@ var enableCheckboxList = function() {
 
 		// this is also called when pressing the new-item 'item'
 		// it probably shouldn't
-		post("http://localhost:3000/note/list/set-checkbox",
+		post("/note/list/set-checkbox",
 				{
 					"list-id": el.data("listId"),
 					"id": el.data("id"),
@@ -53,7 +53,7 @@ var newBulletSubmit = function(event) {
 	var text = textArea.val();
 	var listId = form.data("listId");
 	
-	post("http://localhost:3000/note/list/add-item",
+	post("/note/list/add-item",
 			{
 				"list-id": listId,
 				"text": text
@@ -86,7 +86,7 @@ var createNote = function(json) {
 	$("#note-container").prepend(template(json));
 
 	if(json.type == "list") {
-		$.getJSON("http://localhost:3000/note/list?id="+json.id, function(list) {
+		$.getJSON("/note/list?id="+json.id, function(list) {
 			$("#note-" + json.id).children(".note-footer").before(createHTMLList(list));
 			// TODO possibly check this so it doesn't reaply the function to all forms
 			$("form[name=new-bullet]").submit(newBulletSubmit);
@@ -101,7 +101,7 @@ var getNotes = function(url) {
 }
 
 var getNotesById = function(id) {
-	var url = "http://localhost:3000/note?id=" + id;
+	var url = "/note?id=" + id;
 	getNotes(url);
 }
 
@@ -142,7 +142,7 @@ $(document).ready(function() {
 	});
 
 	// add all notes from the server to start with
-	getNotes("http://localhost:3000/note/all");
+	getNotes("/note/all");
 
 	// TODO this is uggly!
 	setTimeout(afterInit, 1500);
